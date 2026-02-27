5b2dae47cc30359fef26f43fad7b6bd02639cc27
alburquenqueletelier <alburquenque.letelier@gmail.com>
2026-02-27 10:33:35 -0300

    ahora expone servicio en todas las interfaces

--- landing/vite.config.ts
+++ landing/vite.config.ts
@@ -4,6 +4,9 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
